d1ace05251cf49aa1d52d1dfe279a0a03eedc37e
Enzzo657 <kapanirovam@gmail.com>
2024-12-08 17:36:40 +0000

    app.py:add autoincrement=True

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 
 # Модель таблицы пользователей
 class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
